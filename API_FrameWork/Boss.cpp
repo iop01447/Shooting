a2c8d11e97cb286439d9d23e138c4e764d6db71f
tmdgh <tmdgh4739@gmail.com>
2020-02-05 20:56:30 +0900

    asd

---
+++ API_FrameWork/Boss.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Boss.h"
+
+
+CBoss::CBoss()
+{
+}
+
+
+CBoss::~CBoss()
+{
+}
---
+++ API_FrameWork/Boss.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Obj.h"
+class CBoss :
+	public CObj
+{
+public:
+	CBoss();
+	virtual ~CBoss();
+
+	virtual void Initialize();
+	virtual int Update();
+	virtual void Late_Update();
+	virtual void Render(HDC _DC);
+	virtual void Release();
+};
+
